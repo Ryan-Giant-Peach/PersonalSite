deda43220153475a9dffce54062f6299b19e906c
Ryan Caulfield <Ryancaulfield3103@gmail.com>
2022-05-22 15:11:25 +0100

    initial styling to personal site with styled components installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Title>
+        <h1>Ryan Caulfield</h1>
+        <h2>Software Engineer</h2>
+      </Title>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  color: salmon;
+  background-color: #3A3A3D;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  align-items: center;
+  border: 10px solid salmon;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  font-family: "Roboto", sans-serif;
+  h1 {
+    font-size: 5rem;
+    color: salmon;
+  }
+  h2 {
+    font-size: 2rem;
+    color: salmon;
+  }
+`;
